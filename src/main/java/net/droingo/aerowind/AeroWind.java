57298916d876e213596d20b00bc6c64822f579c4
Droingo <cmbmx57@gmail.com>
2026-05-15 19:55:14 +1000

    clean up

---
+++ src/main/java/net/droingo/aerowind/AeroWind.java
@@ -0,0 +1,16 @@
+package net.droingo.aerowind;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(AeroWind.MOD_ID)
+public final class AeroWind {
+    public static final String MOD_ID = "aero_wind";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AeroWind(IEventBus modEventBus) {
+        LOGGER.info("Aero Wind loaded");
+    }
+}
